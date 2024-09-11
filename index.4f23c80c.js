86951eea61561403844b5a2c89c424328e4cbb31
Diana Darienko <diana.darienko21@gmail.com>
2024-09-11 12:35:39 +0300

    make build

---
+++ index.4f23c80c.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link").getAttribute("href");e.setAttribute("src",r)});
+//# sourceMappingURL=index.4f23c80c.js.map
